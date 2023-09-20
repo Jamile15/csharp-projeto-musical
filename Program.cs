ad84355a67379d3c5cf64b312728a71e415a5c66
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-09-20 10:36:50 -0300

    subindo menu

--- Program.cs
+++ Program.cs
@@ -1,13 +1,26 @@
 ﻿//Screen Sound
 
-string mensagemDeBoasVindas ="boas vindas ao screen sound";
+string mensagemDeBoasVindas ="Não to pronta pra essa semana";
 
 
 void ExibirMensagemDeBoasVindas(){
-    Console.WriteLine("**********************************");
+    Console.WriteLine(@"
+█▀█ █░█ █▀▀ █▀█ █▀█   █▀▄▀█ █ █▄░█ █░█ ▄▀█   █▀▀ ▄▀█ █▀▄▀█ ▄▀█
+▀▀█ █▄█ ██▄ █▀▄ █▄█   █░▀░█ █ █░▀█ █▀█ █▀█   █▄▄ █▀█ █░▀░█ █▀█
+");
     Console.WriteLine(mensagemDeBoasVindas);
-    Console.WriteLine("**********************************");
-
 }
 
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("\nDigirte 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("digite 3 para avaliar uma banda");
+    Console.WriteLine("digite 4 para exibir uma media de uma banda");
+    Console.WriteLine("digite -1 para sair");
+
+
+};
+
 ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
