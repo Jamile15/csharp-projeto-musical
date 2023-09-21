666527fadd105381fa81a59a2978edd026f3e5e9
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-09-21 09:35:03 -0300

    teste

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 ﻿//Screen Sound
 
-string mensagemDeBoasVindas ="Não to pronta pra essa semana";
+string mensagemDeBoasVindas = "Não to pronta pra essa semana";
 
 
-void ExibirMensagemDeBoasVindas(){
+void ExibirMensagemDeBoasVindas()
+{
     Console.WriteLine(@"
 █▀█ █░█ █▀▀ █▀█ █▀█   █▀▄▀█ █ █▄░█ █░█ ▄▀█   █▀▀ ▄▀█ █▀▄▀█ ▄▀█
 ▀▀█ █▄█ ██▄ █▀▄ █▄█   █░▀░█ █ █░▀█ █▀█ █▀█   █▄▄ █▀█ █░▀░█ █▀█
@@ -21,25 +22,32 @@ void ExibirOpcoesDoMenu()
 
     Console.Write("\n digite a sua opção:");
 
-    ///pegar o valor que for digitado 
-     string opcaoEscolhida =  Console.ReadLine()!;
-     //convertendo a string da opção escolhida pra um valor inteiro 
-     int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+    
 
-     // ao invez de ficar usando varios if else uso o switch passao o parametro e uso o case pra tipo "caso o 1 seja digitado retorne 
-    switch(opcaoEscolhidaNumerica)
+    ///pegar o valor que for digitado 
+    string opcaoEscolhida = Console.ReadLine()!;
+    //convertendo a string da opção escolhida pra um valor inteiro 
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    // ao invez de ficar usando varios if else uso o switch passao o parametro e uso o case pra tipo "caso o 1 seja digitado retorne 
+    switch (opcaoEscolhidaNumerica)
     {
-        case 1 :  Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
-           break;
-           case 2 : Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
-           break;
-            case 4 : Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
-           break;
-            case -1 : Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
-           break;
-           default: Console.WriteLine("Opção invalida");
-           break;
-        
+        case 1:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 2:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 4:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case -1:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        default:
+            Console.WriteLine("Opção invalida");
+            break;
+
     }
     Console.WriteLine("tchau tchau :)");
 }
